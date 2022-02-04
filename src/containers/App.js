033ca3be7a8e6b76e4a1c4f7d85200adaaddf5b0
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-04 12:48:34 -0500

    Add Error Boundaries for error handling

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import "./App.css";
 import Scroll from "../components/Scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 
 class App extends Component {
   constructor() {
@@ -41,7 +42,9 @@ class App extends Component {
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
       </div>
     );
