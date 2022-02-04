f6fafc6d8d2be09baa853c7c86c73ade28d8f52f
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-03 22:00:56 -0500

    Create Cards for robots

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Wateen",
+    username: "twateen",
+    email: "something@something.com",
+  },
+  {
+    id: 2,
+    name: "Wateen2",
+    username: "twateen2",
+    email: "something2@something.com",
+  },
+  {
+    id: 3,
+    name: "Wateen3",
+    username: "twateen3",
+    email: "something3@something.com",
+  },
+];
