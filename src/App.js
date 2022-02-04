56d7d63d3ffa73a3dc1f4151b7cb47320a2413f1
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-04 01:27:36 -0500

    Fetched robot users from server instead of hard-coding robot names

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import { robots } from "./robots";
+// import { robots } from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import "./App.css";
-
-const state = { robots: robots, searchField: "" };
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange = (event) => {
@@ -25,13 +33,18 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc"> Loading ...</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
