b6d67eb972a34a7664799edb542170873a12c96e
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-04 00:56:11 -0500

    Update title styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { robots } from "./robots";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+import "./App.css";
 
 const state = { robots: robots, searchField: "" };
 
@@ -26,7 +27,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
