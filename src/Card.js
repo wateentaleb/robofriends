6ca0cfb6ef453610d3d79e16833605adb3e0fec1
Wateen Taleb <wateentaleb@MacBook-Pro.local>
2022-02-04 12:12:54 -0500

    Update Card style to gradient

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,10 @@
 import React from "react";
 
+import "./Card.css";
+
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div id="Card" className="tc dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="Robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
